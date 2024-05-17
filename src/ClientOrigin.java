6c392d034edfa5d3673951429ded8914c0f03305
22fi065 <166772094+22fi065@users.noreply.github.com>
2024-05-17 11:03:19 +0900

    korede

--- src/ClientOrigin.java
+++ src/ClientOrigin.java
@@ -15,27 +15,35 @@ public class ClientOrigin {
             int port = scanner.nextInt();
             System.out.println("localhostの" + port + "番ポートに接続を要求します");
             Socket socket = new Socket("localhost", port);
+
             System.out.println("接続されました");
+
+            System.out.println("終了したい場合はowariと入力してください、はい/いいえを入力");
+
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 
             while (p != scanner.next()) {
-
-                System.out.println("プレゼントの内容を入力してください(例:お菓子) ↓");
-                String content = scanner.next();
+                System.out.println("一つ目の数字を入力");
 
                 XmasPresent present = new XmasPresent();
-                present.setContent(content);
-                System.out.println("korosuzo");
-                oos.writeObject(present);
-                System.out.println("クソボケがーっ");
+                present.setContent(scanner.nextInt());
+                System.out.println("二つ目の数字を入力");
 
+                int suuji2 = scanner.nextInt();
+                present.setSuuji2(suuji2);
+
+                System.out.println("演算子の入力");
+
+                present.setEnzanshi(scanner.next());
+                System.out.println("入力が完了しました、serverを確認してください。再計算をする場合は数字を入力してください");
+                oos.writeObject(present);
                 oos.flush();
                 // このタイミングで送信してそう？
-                oos.close();
+                // oos.close();
 
             }
+            oos.close();
             scanner.close();
-
             socket.close();
         } catch (BindException be) {
             be.printStackTrace();
